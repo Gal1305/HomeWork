8e4b34a097bb8bda666dc5991c14d2df6e65b23b
Gal1305 <lollolloll12313@gmail.com>
2024-11-09 16:03:48 +0500

    correction

--- Module_2_4_Cycle_for.py
+++ Module_2_4_Cycle_for.py
@@ -2,18 +2,15 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 for q in numbers:
-    print(q)
-for q in numbers:
-    if q <= 1:
+    if q < 2:
         continue
-    elif q == 2:
+    is_primes = True
+    for i in range(2, int(q ** 0.5) +1):
+        if q % i == 0:
+            is_primes = False
+            not_primes.append(q)
+            break
+    if is_primes:
         primes.append(q)
-    elif q == 3:
-        primes.append(q)
-    elif q % 2 == 0:
-        not_primes.append(q)
-    elif q % 3 == 0:
-        not_primes.append(q)
-    else: primes.append(q)
 print(primes, ' - список простых чисел')
 print(not_primes, ' - список составных чисел')
